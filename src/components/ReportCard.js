d483ebd31ac3dc9bb6874fb458aebd6714c272b2
Wietse Neven <hello@wietseneven.com>
2017-03-30 10:42:44 -0500

    Integration of a good leaderboard

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -5,11 +5,11 @@ import firebase from 'firebase';
 require('styles/ReportCard.scss');
 
 const config = {
-  apiKey: "AIzaSyBl-pcS2XqyKb-79QuQvsIwsWJ1Zx693i4",
-  authDomain: "detector-de-mentiras-69bb7.firebaseapp.com",
-  databaseURL: "https://detector-de-mentiras-69bb7.firebaseio.com",
-  storageBucket: "detector-de-mentiras-69bb7.appspot.com",
-  messagingSenderId: "433947191175"
+  apiKey: 'AIzaSyBl-pcS2XqyKb-79QuQvsIwsWJ1Zx693i4',
+  authDomain: 'detector-de-mentiras-69bb7.firebaseapp.com',
+  databaseURL: 'https://detector-de-mentiras-69bb7.firebaseio.com',
+  storageBucket: 'detector-de-mentiras-69bb7.appspot.com',
+  messagingSenderId: '433947191175'
 };
 let firebaseDone = false;
 class ReportCardComponent extends React.Component {
@@ -19,11 +19,17 @@ class ReportCardComponent extends React.Component {
 
     this.state = {
       userResult: {},
+      allScores: [],
+      username: '',
       openRow: 999,
-      finalScore: 0
+      finalScore: 0,
+      leaderScore: '...',
+      submitted: false
     };
 
     this.openRow = this.openRow.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.submitToLeaderBoard = this.submitToLeaderBoard.bind(this);
     this.handleFacebookshare = this.handleFacebookshare.bind(this);
     this.handleTwitterShare = this.handleTwitterShare.bind(this);
   }
@@ -41,16 +47,18 @@ class ReportCardComponent extends React.Component {
       firebase.initializeApp(config);
       firebaseDone = true;
     }
+    this.getLeaderBoard();
   }
 
   componentWillReceiveProps() {
     this.getUserScore();
+    this.getLeaderBoard();
   }
 
   getAllQuestions() {
     return this.props.questions.map((question, index) => {
       let open = false;
-      if (this.state.openRow == index) {
+      if (this.state.openRow === index) {
         open = true;
       }
 
@@ -133,30 +141,133 @@ class ReportCardComponent extends React.Component {
     }
   }
 
+  getForm() {
+    if (this.state.submitted) {
+      return (
+        <em>Succesfully submitted your name to the leaderboard</em>
+      )
+    } else {
+      return (
+        <form onSubmit={this.submitToLeaderBoard}>
+          <p>Want to be on our leaderboard?</p>
+          <div className="ReportCard__form__group">
+            <input type="text" value={this.state.username} onChange={this.handleNameChange} placeholder="Tu nombre" />
+            <input type="submit" value="submit" />
+          </div>
+        </form>
+      );
+    }
+  }
+
+  getLeaders() {
+    const scores = this.state.allScores.sort(dynamicSort('result')).reverse();
+    return scores.map((score, index) => {
+      return (
+        <tr key={index}>
+          <td width="75%">{score.name}</td>
+          <td width="25%">{score.result}</td>
+        </tr>
+      )
+    });
+  }
+
   render() {
+
+    const form = this.getForm();
+    const leaderboard = this.getLeaders();
+
+    let score = this.state.userResult.point;
+    if (score < 0) score = 0;
+
     return (
       <div className="ReportCard">
         <h3 className="Question__title">Resultado</h3>
         <span><strong>{this.state.userResult.name}</strong></span>
+        <p>Tu tienes un resultado de {score}/100.</p>
 
-        {/*<img src={} alt={this.state.userResult.name} />*/}
         <div className="ReportCard__social row">
-          <h5><em>Comparte tu resultado en:</em></h5>
-          <button className="ReportCard__social-btn ReportCard__social-btn--facebook"
-                  onClick={this.handleFacebookshare}>Facebook
-          </button>
-          <button className="ReportCard__social-btn ReportCard__social-btn--twitter"
-                  onClick={this.handleTwitterShare}>Twitter
-          </button>
+            <div className="col-sm-6">
+            <h5>¿Quién es el mejor detector de mentiras?</h5>
+
+            <div className="ReportCard__form">
+              {form}
+            </div>
+
+            <h5>Nosotros top 25</h5>
+            <div className="ReportCard__table">
+              <table className="table leaderboard-table">
+                <thead>
+                <tr>
+                  <th width="75%">Nombre</th>
+                  <th width="25%">Score</th>
+                </tr>
+                </thead>
+              </table>
+              <div className="ReportCard__table--scroll">
+                <table className="table leaderboard-table">
+                  <tbody>
+                  {leaderboard}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6">
+            <h5>Comparte tu resultado en:</h5>
+            <div className="row">
+              <img
+                width="300"
+                src={`https://github.com/La-Silla-Vacia/fact-quiz/raw/master/src/images/resultados/${this.state.userResult.shortName}.png`}
+                alt={this.state.userResult.name}
+                className="col-xs-12"
+              />
+            </div>
+            <footer className="ReportCard__footer">
+              <button className="ReportCard__social-btn ReportCard__social-btn--facebook"
+                      onClick={this.handleFacebookshare}>Facebook
+              </button>
+              <button className="ReportCard__social-btn ReportCard__social-btn--twitter"
+                      onClick={this.handleTwitterShare}>Twitter
+              </button>
+            </footer>
+          </div>
         </div>
       </div>
     );
   }
 
+  handleNameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  submitToLeaderBoard(e) {
+    e.preventDefault();
+    if (!this.state.username) return;
+    const messageListRef = firebase.database().ref('leaderboard');
+    const newMessageRef = messageListRef.push();
+    newMessageRef.set({
+      'timestamp': Math.floor(Date.now() / 1000),
+      'result': this.state.userResult.point,
+      'name': this.state.username
+    });
+
+    this.setState({ submitted: true })
+  }
+
+  getLeaderBoard() {
+    const allScores = [];
+    const scoresRef = firebase.database().ref('leaderboard');
+    scoresRef.orderByChild('result').limitToLast(25).on('child_added', (snapshot) => {
+      allScores.push(snapshot.val());
+    });
+    this.setState({ allScores: allScores });
+  }
+
   handleTwitterShare(event) {
     event.preventDefault();
 
-    window.open('https://twitter.com/intent/tweet?text='+ this.state.userResult.name +' en La Silla Vacía! ¿Y Usted?&url=http://lasillavacia.com/detector-de-mentiras',
+    window.open('https://twitter.com/intent/tweet?text=' + this.state.userResult.name + ' en La Silla Vacía! ¿Y Usted?&url=http://lasillavacia.com/detector-de-mentiras',
       '_blank',
       'toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450');
   }
@@ -174,19 +285,31 @@ class ReportCardComponent extends React.Component {
     FB.ui({
         // ID de la cuenta de Jerrejerre, cambiar por la de LSV
         //app_id: "866382070105753",
-        app_id: "707991449304858",
+        app_id: '707991449304858',
         method: 'feed',
         link: 'http://lasillavacia.com/detector-de-mentiras',
         picture: `https://github.com/La-Silla-Vacia/fact-quiz/raw/master/src/images/resultados/${this.state.userResult.shortName}.png`,
         name: 'Detector de mentiras - La Silla Vacia'
       },
-      function (response) {
+      function () {
         /*if (response && response.post_id) {
          alert('Post was published.');
          } else {
          alert('Post was not published.');
          }*/
       });
+  }
+}
+
+function dynamicSort(property) {
+  let sortOrder = 1;
+  if (property[0] === '-') {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    return result * sortOrder;
   }
 }
 
